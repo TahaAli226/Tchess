e4bf0a1ffd1ad0753ae67eb8d1355b36be68c519
Taha <syncting@DESKTOP-B9BKMVP.localdomain>
2026-09-22 18:07:26 +0300

    Phase 0:added the pieces

--- board.py
+++ board.py
@@ -1,6 +1,19 @@
+from pieces import Pawn,Rook,Knight,Bishop,Queen,King
+
+
 class Board:
     def __init__(self):
         self.__grid = [[None for x in range(8)] for x in range(8)]
+        self.place_pieces()
+        
+    def place_pieces(self):
+        pieces = [Rook,Knight,Bishop,Queen,King,Bishop,Knight,Rook]
+        for index, piece in enumerate(pieces):
+            self.__grid[0][index] = piece("black")
+            self.__grid[7][index] = piece("white")
+        for index in range(8):
+            self.__grid[1][index] = Pawn("black") 
+            self.__grid[6][index] = Pawn("white")
 
     def display_board(self):
         letters = [" ","a","b","c","d","e","f","g","h"]
@@ -13,7 +26,7 @@ class Board:
                     symbols.append(".")
 
                 else:
-                    symbols.append(str(cell))
+                    symbols.append(cell.shape())
             print(f"{rank} {" ".join(symbols)} {rank}")
         print(" ".join(letters))
 
@@ -21,4 +34,3 @@ if __name__ == "__main__":
     
     test = Board()
     test.display_board()
-    
---
+++ pieces.py
@@ -0,0 +1,27 @@
+class Piece:
+    def __init__(self,color):
+        self._color = color 
+    def shape(self):
+        if self._color == "white":
+            return self.white
+        else:
+            return self.black 
+
+class Pawn(Piece):
+    white = "♟"
+    black = "♙"
+class Rook(Piece):
+    white = "♜"
+    black = "♖"
+class Knight(Piece):
+    white = "♞"
+    black = "♘"
+class Bishop(Piece):
+    white = "♝"
+    black = "♗"
+class Queen(Piece):
+    white = "♛"
+    black = "♕"
+class King(Piece):
+    white = "♚"
+    black = "♔"
